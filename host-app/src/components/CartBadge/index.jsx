0702e0d0818c21522fae466ab7056e11ec59e6ba
Tiago Lima <tiago.lima@osf.digital>
2020-08-05 16:25:32 -0300

    Made a Micro Frontend Sample.

---
+++ host-app/src/components/CartBadge/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+import useMicrofrontend from '../../hooks/useMicrofrontend';
+import config from '../../utils/config';
+
+const CartBadge = ({ cart }) => {
+  let history = useHistory();
+  const [cartNum, setCartNum] = useState(cart.length);
+  const { isLoaded, mfProducts } = useMicrofrontend('mfProducts', config.mfProductsUrl);
+
+  const handleClick = () => history.push('/cart');
+  const handleProductAdded = () => setCartNum(v => v + 1);
+
+  useEffect(() => {
+    setCartNum(cart.length);
+  }, [cart]);
+
+  useEffect(() => {
+    if (!mfProducts) return;
+    const { customEvents, subscribe, unSubscribe } = mfProducts;
+    subscribe(customEvents.PRODUCTS_MF_PRODUCT_ADDED, handleProductAdded);
+  
+    return () => unSubscribe(customEvents.PRODUCTS_MF_PRODUCT_ADDED, handleProductAdded);
+  }, [isLoaded, mfProducts]);
+
+  return (
+    <IconButton onClick={handleClick} aria-label={`show ${cartNum} products`}>
+      <Badge badgeContent={cartNum} color="secondary">
+        <ShoppingCartIcon />
+      </Badge>
+    </IconButton>
+  )
+};
+
+export default CartBadge;
---
+++ setup.js
@@ -0,0 +1,85 @@
+var fs = require("fs");
+var resolve = require("path").resolve;
+var join = require("path").join;
+var cp = require("child_process");
+var os = require("os");
+
+// get library path
+
+var root = resolve(__dirname, ".");
+
+// npm binary based on OS
+
+var npmCmd = os.platform().startsWith("win") ? "npm.cmd" : "npm";
+
+fs.readdirSync(root).forEach(function (subDir) {
+  var subDirPath = join(root, subDir);
+
+  // ensure path has package.json --> skip backend folder
+
+  if (!fs.existsSync(join(subDirPath, "package.json"))) return;
+
+  console.log(
+    "==================================================================="
+  );
+  console.log(`Performing "npm install" inside ${subDir} folder`);
+  console.log(
+    "==================================================================="
+  );
+
+  // install dependencies
+
+  cp.spawnSync(npmCmd, ["ci"], {
+    env: process.env,
+    cwd: subDirPath,
+    stdio: "inherit",
+  });
+
+  // build if needed
+
+  if (subDir === "host-app" || subDir === "backend") return;
+
+  console.log(
+    "==================================================================="
+  );
+  console.log(`Build app inside ${subDir} folder`);
+  console.log(
+    "==================================================================="
+  );
+
+  cp.spawnSync(npmCmd, ["run", "build:local"], {
+    env: process.env,
+    cwd: subDirPath,
+    stdio: "inherit",
+  });
+});
+
+// Running the app
+
+console.log(
+  "==================================================================="
+);
+console.log(`Starting the json-server and the app`);
+console.log(
+  "==================================================================="
+);
+
+// backend
+
+var backendPath = resolve(__dirname, "./backend");
+
+cp.spawn(npmCmd, ["start"], {
+  env: process.env,
+  cwd: backendPath,
+  stdio: "inherit",
+});
+
+// host-app
+
+var hostAppPath = resolve(__dirname, "./host-app");
+
+cp.spawn(npmCmd, ["start"], {
+  env: process.env,
+  cwd: hostAppPath,
+  stdio: "inherit",
+});
